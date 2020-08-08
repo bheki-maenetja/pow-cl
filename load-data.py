8614ed9b17680b917e7e51d2f359eee230f2146c
bheki-maenetja <bhekimaenetja@gmail.com>
2020-08-08 13:39:10 +0200

    modified json formatting

--- load-data.py
+++ load-data.py
@@ -1,27 +1,29 @@
 import requests
 import json
 
-api_url = "https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/id/1.json"
+api_url = "https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/id/208.json"
 
 data = requests.get(api_url)
 json_data = data.json()
 
 for i in json_data:
   if i == 'appearance':
-    appearance = json_data[i]
+    appearance = json_data[i].copy()
     appearance['height'] = f"{appearance['height'][1]} ({appearance['height'][0]})" 
     appearance['weight'] = f"{appearance['weight'][1]} ({appearance['weight'][0]})"
     appearance['eye_colour'] = appearance['eyeColor']
     appearance['hair_colour'] = appearance['hairColor'] if appearance['hairColor'] != "No Hair" else "n/a"
-    del appearance['eyeColor']
-    del appearance['hairColor']
+    del appearance['eyeColor'], appearance['hairColor']
 
-    for key in appearance:
-      appearance[key] = appearance[key].lower()
+    json_data[i] = { key : appearance[key].lower() for key in appearance }
   elif i == 'biography':
-    biography = json_data[i]
+    biography = json_data[i].copy()
     biography['full_name'] = biography['fullName'] if biography['fullName'] else "n/a"
-    del biography['fullName']
+    biography['place_of_birth'] = biography['placeOfBirth'] if biography['placeOfBirth'] != "-" else "n/a"
+    biography['first_appearance'] = biography['firstAppearance'] if biography['firstAppearance'] != "-" else "n/a"
+    biography['alter_egos'] = biography['alterEgos'] if biography['alterEgos'] != "No alter egos found." else "n/a"
+    biography['aliases'] = [alias.lower() for alias in biography['aliases'] if alias != '-']
+    del biography['fullName'], biography['placeOfBirth'], biography['firstAppearance'], biography['alterEgos']
 
 
 for i in json_data:
