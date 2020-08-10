3f9c5b6d8182f5d1892cd25b7f9fc29b9906d633
bheki-maenetja <bhekimaenetja@gmail.com>
2020-08-10 11:28:03 +0200

    finished basic json formatting

--- load-data.py
+++ load-data.py
@@ -2,45 +2,48 @@ import requests
 import json
 import re
 
-api_url = "https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/id/5.json"
+api_url = "https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/all.json"
 
 data = requests.get(api_url)
 json_data = data.json()
 
-for i in json_data:
-  if i == 'appearance':
-    appearance = json_data[i].copy()
-    appearance['height'] = f"{appearance['height'][1]} ({appearance['height'][0]})" 
-    appearance['weight'] = f"{appearance['weight'][1]} ({appearance['weight'][0]})"
-    appearance['eye_colour'] = appearance['eyeColor']
-    appearance['hair_colour'] = appearance['hairColor'] if appearance['hairColor'] != "No Hair" else "n/a"
-    del appearance['eyeColor'], appearance['hairColor']
+def format_json(json_data):
+  json_data['image'] = json_data['images']['lg']
+  del json_data['images']
 
-    json_data[i] = { key : appearance[key].lower() if appearance[key] else "n/a" for key in appearance }
-  elif i == 'biography':
-    biography = json_data[i].copy()
-    biography['full_name'] = biography['fullName'] if biography['fullName'] else "n/a"
-    biography['place_of_birth'] = biography['placeOfBirth'] if biography['placeOfBirth'] != "-" else "n/a"
-    biography['first_appearance'] = biography['firstAppearance'] if biography['firstAppearance'] != "-" else "n/a"
-    biography['alter_egos'] = biography['alterEgos'] if biography['alterEgos'] != "No alter egos found." else "n/a"
-    biography['aliases'] = [alias.lower() for alias in biography['aliases'] if alias != '-']
-    del biography['fullName'], biography['placeOfBirth'], biography['firstAppearance'], biography['alterEgos']
+  for i in json_data:
+    if i == 'appearance':
+      appearance = json_data[i].copy()
+      appearance['height'] = f"{appearance['height'][1]} ({appearance['height'][0]})" 
+      appearance['weight'] = f"{appearance['weight'][1]} ({appearance['weight'][0]})"
+      appearance['eye_colour'] = appearance['eyeColor']
+      appearance['hair_colour'] = appearance['hairColor'] if appearance['hairColor'] != "No Hair" else "n/a"
+      del appearance['eyeColor'], appearance['hairColor']
 
-    json_data[i] = { key : biography[key].lower() if key != 'aliases' else biography[key] for key in biography }
-  elif i == 'work':
-    work = json_data[i].copy()
-    work['occupation'] = [word.strip().lower() for word in re.split(',|_|-|!|;', work['occupation'])] if work['occupation'] != '-' else 'n/a'
-    work['base'] = work['base'].lower() if work['base'] != "-" else "n/a"
+      json_data[i] = { key : appearance[key].lower() if appearance[key] else "n/a" for key in appearance }
+    elif i == 'biography':
+      biography = json_data[i].copy()
+      biography['full_name'] = biography['fullName'] if biography['fullName'] else "n/a"
+      biography['place_of_birth'] = biography['placeOfBirth'] if biography['placeOfBirth'] != "-" else "n/a"
+      biography['first_appearance'] = biography['firstAppearance'] if biography['firstAppearance'] != "-" else "n/a"
+      biography['alter_egos'] = biography['alterEgos'] if biography['alterEgos'] != "No alter egos found." else "n/a"
+      biography['aliases'] = [alias.lower() for alias in biography['aliases']] if biography['aliases'] != ['-'] else 'n/a'
+      del biography['fullName'], biography['placeOfBirth'], biography['firstAppearance'], biography['alterEgos']
 
-    json_data[i] = { key: work[key] for key in work }
-  elif i == 'connections':
-    connections = json_data[i].copy()
-    connections['group_affiliations'] = [word.strip().lower() for word in re.split(',|_|-|!|;', connections['groupAffiliation'])] if connections['groupAffiliation'] != '-' else 'n/a'
-    connections['relatives'] = [word.strip().lower() for word in re.split(',|_|-|!|;', connections['relatives'])] if connections['relatives'] != '-' else 'n/a'
-    del connections['groupAffiliation']
-    json_data[i] = connections
+      json_data[i] = { key : biography[key].lower() if key != 'aliases' else biography[key] for key in biography }
+    elif i == 'work':
+      work = json_data[i].copy()
+      work['occupation'] = [word.strip().lower() for word in re.split(',|_|-|!|;', work['occupation'])] if work['occupation'] != '-' else 'n/a'
+      work['base'] = work['base'].lower() if work['base'] != "-" else "n/a"
+
+      json_data[i] = { key: work[key] for key in work }
+    elif i == 'connections':
+      connections = json_data[i].copy()
+      connections['group_affiliations'] = [word.strip().lower() for word in re.split(',|_|-|!|;', connections['groupAffiliation'])] if connections['groupAffiliation'] != '-' else 'n/a'
+      connections['relatives'] = [word.strip().lower() for word in re.split(',|_|-|!|;', connections['relatives'])] if connections['relatives'] != '-' else 'n/a'
+      del connections['groupAffiliation']
+
+      json_data[i] = connections
 
 
-for i in json_data:
-  print(i)
-  print(json_data[i], end="\n\n")
+print(json_data)
