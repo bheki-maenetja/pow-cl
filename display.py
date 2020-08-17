126279dc3c1bd759498d7d3df5ce488d829e906a
bheki-maenetja <bhekimaenetja@gmail.com>
2020-08-17 14:19:09 +0200

    installed pygame

--- display.py
+++ display.py
@@ -1,6 +1,7 @@
 # Standard Library Imports
 # Third Party Imports
 from tabulate import tabulate
+import pygame
 # Local Imports
 
 # Get Search Table -- returns tabulated search data
@@ -11,3 +12,6 @@ def get_search_table(search_data):
   formatted_data = [{ key.upper(): obj[key] for key in obj if key != 'id' } for obj in search_data]
   tabulated_data = tabulate(formatted_data, headers="keys", showindex=range(1, len(formatted_data) + 1))
   print(tabulated_data, end="\n\n")
+
+def set_up_window():
+  pass
