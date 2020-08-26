5dc796884cbcaf07b58001f391e5bd81b3665da4
bheki-maenetja <bhekimaenetja@gmail.com>
2020-08-26 14:35:39 +0200

    moved search files into their own directory

--- search/search_table.py
+++ search/search_table.py
@@ -11,5 +11,4 @@ def get_search_table(search_data):
   formatted_data = [{ key.upper(): obj[key] for key in obj if key != 'id' } for obj in search_data]
   tabulated_data = tabulate(formatted_data, headers="keys", showindex=range(1, len(formatted_data) + 1))
   print(tabulated_data, end="\n\n")
-
-# Display superhero information in a new pygame window
+ 
