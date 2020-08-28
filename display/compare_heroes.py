b9d904f001bef2503c09638988509d7f4ef79645
bheki-maenetja <bhekimaenetja@gmail.com>
2020-08-28 16:06:28 +0200

    rendered hero names on comparison tools

--- display/compare_heroes.py
+++ display/compare_heroes.py
@@ -41,8 +41,12 @@ def display_heroes():
     # Update
     # Draw / Render
     screen.fill(BACKGROUND_COLOUR)
+
     screen.blit(hero_images[0], (WIDTH // 4 - 150, 10))
     screen.blit(hero_images[1], (3 * WIDTH // 4 - 150, 10))
+
+    draw_text(screen, hero_data[0]['name'].title(), 36, WIDTH // 4, 435, align_centre=True)
+    draw_text(screen, hero_data[1]['name'].title(), 36, 3 * WIDTH // 4, 435, align_centre=True)
     # AFTER Drawing Everything, Flip the Display
     pygame.display.flip()
 
@@ -63,5 +67,17 @@ def load_hero_image(image_url):
   image_file = io.BytesIO(image_str)
   hero_image = pygame.image.load(image_file)
   return pygame.transform.scale(hero_image, (300,400))
+
+# Text handling - writes text on the pygame window
+def draw_text(surf, text, font_size, x, y, font_name='arial', font_colour=colours.BLACK, align_centre=False):
+  chosen_font = pygame.font.match_font(font_name)
+  font = pygame.font.Font(chosen_font, font_size)
+  text_surf = font.render(text, True, font_colour)
+  text_rect = text_surf.get_rect()
+  if align_centre:
+    text_rect.center = (x,y)
+  else:
+    text_rect.x, text_rect.y = x, y
+  surf.blit(text_surf, text_rect)
 
 display_heroes()
