c4cb3e92f8cc7b16657334409b953a107a795e15
bheki-maenetja <bhekimaenetja@gmail.com>
2020-08-28 15:54:55 +0200

    implemented functionality to load heroes on hero comparision display

--- display/compare_heroes.py
+++ display/compare_heroes.py
@@ -13,12 +13,16 @@ import colours
 
 def display_heroes():
   WIDTH, HEIGHT = 540, 720
-  BACKGROUND_COLOUR = colours.GREEN
+  BACKGROUND_COLOUR = colours.BLUE
   FPS = 30
   
   pygame.init()
   screen = pygame.display.set_mode((WIDTH, HEIGHT))
   clock = pygame.time.Clock()
+
+  hero_data = load_hero_data()
+  for hero in hero_data:
+    print(type(hero))
 
   # GAME LOOP
   running = True
@@ -40,5 +44,15 @@ def display_heroes():
 
     # Keep Game Loop Running at Given FPS
     clock.tick(FPS)
+
+# Load hero info - loads hero information from the current_hero file
+def load_hero_data():
+  try:
+    file_handler = open(path.join(path.dirname(__file__), argv[1]), 'r')
+    data = [loads(line) for line in file_handler.readlines()]
+    # hero_data = loads(data)
+    return data
+  except:
+    print('Could not load hero info')
 
 display_heroes()
