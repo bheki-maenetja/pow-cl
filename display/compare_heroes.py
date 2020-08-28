fb5cfb8de9fa542e16810c55cec1bec9d0fdd2fd
bheki-maenetja <bhekimaenetja@gmail.com>
2020-08-28 16:03:09 +0200

    rendered images on hero comparision display

--- display/compare_heroes.py
+++ display/compare_heroes.py
@@ -12,7 +12,7 @@ import pygame
 import colours
 
 def display_heroes():
-  WIDTH, HEIGHT = 540, 720
+  WIDTH, HEIGHT = 960, 720
   BACKGROUND_COLOUR = colours.BLUE
   FPS = 30
   
@@ -21,6 +21,8 @@ def display_heroes():
   clock = pygame.time.Clock()
 
   hero_data = load_hero_data()
+  hero_images = [load_hero_image(hero['image']) for hero in hero_data]
+  
   for hero in hero_data:
     print(type(hero))
 
@@ -39,6 +41,8 @@ def display_heroes():
     # Update
     # Draw / Render
     screen.fill(BACKGROUND_COLOUR)
+    screen.blit(hero_images[0], (WIDTH // 4 - 150, 10))
+    screen.blit(hero_images[1], (3 * WIDTH // 4 - 150, 10))
     # AFTER Drawing Everything, Flip the Display
     pygame.display.flip()
 
@@ -50,9 +54,14 @@ def load_hero_data():
   try:
     file_handler = open(path.join(path.dirname(__file__), argv[1]), 'r')
     data = [loads(line) for line in file_handler.readlines()]
-    # hero_data = loads(data)
     return data
   except:
     print('Could not load hero info')
+
+def load_hero_image(image_url):
+  image_str = urlopen(image_url).read()
+  image_file = io.BytesIO(image_str)
+  hero_image = pygame.image.load(image_file)
+  return pygame.transform.scale(hero_image, (300,400))
 
 display_heroes()
